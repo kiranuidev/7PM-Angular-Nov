e21615e681d5701edfc887dd26a2b2b13c153625
kiranpaturi <kiran_p@k3informatics.com>
2016-11-24 20:38:17 +0530

    Initial Checkin

---
+++ Angular2Proj/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template:`<h1 class="heading-class">{{title}}</h1>`,
+  //templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+}
